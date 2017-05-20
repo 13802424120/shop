5c72ac4c52b843d4c92f99213ccd006bc37274df
hhl <shzphp@qq.com>
2017-05-20 18:57:25 +0800

    修改视图

--- resources/views/sort/lst.blade.php
+++ resources/views/sort/lst.blade.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html lang="zh-cn">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-    <meta name="renderer" content="webkit">
-    <title></title>
-    <link rel="stylesheet" href="{{ asset('css/pintuer.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
-    <script src="{{ asset('js/jquery.js') }}"></script>
-    <script src="{{ asset('js/pintuer.js') }}"></script>
-</head>
-<body>
+@extends('layouts.admin')
+@section('content')
 <div class="panel admin-panel">
     <div class="panel-head"><strong class="icon-reorder"> 分类列表</strong></div>
     <div class="padding border-bottom">
@@ -47,5 +35,4 @@
         }
     }
 </script>
-</body>
-</html>
+@endsection
